d09dd7ad904f03c3c34966e80159b1a2d0a65d38
gokulnpc <gokulnpc@gmail.com>
2024-03-03 21:34:16 +0530

    bg

--- src/QuestionComponent.js
+++ src/QuestionComponent.js
@@ -4,7 +4,7 @@ import './QuestionComponent.css'; // Import the CSS file here
 
 const QuestionComponent = ({ question, options, onOptionSelect }) => {
     return (
-        <div className="question-container"> {/* Use the class name here */}
+        <div className="question-container background"> {/* Use the class name here */}
             <h2 className="question">{question}</h2> {/* Use the class name here */}
             <div className="options"> {/* Use the class name here */}
                 {options.map((option, index) => (
